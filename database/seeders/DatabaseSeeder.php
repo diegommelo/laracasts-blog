ebfc70a4cd6af11622fbc08ac00e00a936434326
Diego Melo <diego.jackchan@gmail.com>
2022-05-04 09:51:07 -0300

    first

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {   
+
+        Post::factory(5)->create();
+   }
+}
---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('posts', [
+        'posts' => Post::latest()->with('category', 'author')->get()
+    ]);
+});
+
+Route::get('posts/{post}', function(Post $post) {
+    return view('post', [
+        'post' => $post
+    ]);
+});
+
+Route::get('categories/{category:slug}', function(Category $category) {
+    return view('posts', [
+        'posts' => $category->posts->load(['category', 'author']) 
+    ]);    
+});
+
+Route::get('author/{author:username}', function(User $author) {
+    return view('posts', [
+        'posts' => $author->posts->load(['category', 'author']) 
+    ]);    
+});
