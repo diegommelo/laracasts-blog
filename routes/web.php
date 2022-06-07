035ac0e4a1f9b3c97d104d59d18a1edc73c3f195
Diego Melo <diego.jackchan@gmail.com>
2022-06-07 14:24:06 -0300

    dropdown blade component

--- routes/web.php
+++ routes/web.php
@@ -18,24 +18,28 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('posts', [
-        'posts' => Post::latest()->with('category', 'author')->get()
+        'posts' => Post::latest()->with('category', 'author')->get(),
+        'categories' => Category::all()        
     ]);
-});
+})->name('home');
 
 Route::get('posts/{post}', function(Post $post) {
     return view('post', [
-        'post' => $post
+        'post' => $post,
     ]);
 });
 
 Route::get('categories/{category:slug}', function(Category $category) {
     return view('posts', [
-        'posts' => $category->posts->load(['category', 'author']) 
-    ]);    
-});
+        'posts' => $category->posts->load(['category', 'author']),
+        'currentCategory' => $category,
+        'categories' => Category::all()        
+    ]);
+})->name('category');
 
 Route::get('author/{author:username}', function(User $author) {
     return view('posts', [
-        'posts' => $author->posts->load(['category', 'author']) 
+        'posts' => $author->posts->load(['category', 'author']),
+        'categories' => Category::all()        
     ]);    
 });
