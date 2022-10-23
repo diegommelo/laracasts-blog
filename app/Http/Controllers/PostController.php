f298e6d0feda6c5498c9c7f4c682c234f118b751
Diego Melo <diego.jackchan@gmail.com>
2022-10-23 14:32:43 -0300

    section 7 - filtering | 40

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,24 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Category;
 use App\Models\Post;
-use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return view('posts', [
+        return view('posts.index', [
             'posts' => Post::latest()->filter(request(['search', 'category']))->get(),
-            'categories' => Category::all(),
-            'currentCategory' => Category::firstWhere('slug', request('category'))
         ]);
     }
 
     public function show(Post $post)
     {
-        return view('post', [
+        return view('posts.show', [
             'post' => $post,
         ]);
     }
