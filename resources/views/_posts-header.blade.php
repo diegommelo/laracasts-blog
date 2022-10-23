cfd755349b84649614ca8afa263eea6fe562f366
Diego Melo <diego.jackchan@gmail.com>
2022-10-22 21:01:44 -0300

    section 7 - filtering | 42

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -18,7 +18,7 @@
                 <x-dropdown-item href="/" :active="request()->routeIs('home')">All</x-dropdown-item>
 
                 @foreach ($categories as $category)
-                <x-dropdown-item href="/categories/{{ $category->slug }}" :active='request()->is("categories/{$category->slug}")'>
+                <x-dropdown-item href="/?category={{ $category->slug }}" :active='request()->is("categories/{$category->slug}")'>
                     {{ ucwords($category->name) }}
                 </x-dropdown-item>
                 @endforeach
